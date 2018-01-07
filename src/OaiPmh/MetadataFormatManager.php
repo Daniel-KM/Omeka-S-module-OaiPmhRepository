0b146a3a60d66c9cc1039b53cfb4cd01c02ea78a
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-01-08 00:00:00 +0100

    Kept oai_dc format first in the list of metadata formats.

--- src/OaiPmh/MetadataFormatManager.php
+++ src/OaiPmh/MetadataFormatManager.php
@@ -7,6 +7,15 @@ use Omeka\ServiceManager\AbstractPluginManager;
 
 class MetadataFormatManager extends AbstractPluginManager
 {
+    /**
+     * Keep oai dc first.
+     *
+     * @var array
+     */
+    protected $sortedNames = [
+        'oai_dc',
+    ];
+
     protected $autoAddInvokableClass = false;
 
     protected $instanceOf = MetadataInterface::class;
