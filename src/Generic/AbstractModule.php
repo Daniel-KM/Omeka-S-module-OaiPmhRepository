62463fd8a74af572cf5ef639983174150f6d99a1
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-11-23 00:00:00 +0000

    Updated Generic module.

--- src/Generic/AbstractModule.php
+++ src/Generic/AbstractModule.php
@@ -66,20 +66,20 @@ abstract class AbstractModule extends \Omeka\Module\AbstractModule
                 $translator->translate('This module requires the module "%s".'), // @translate
                 $this->dependency
             );
-            throw new ModuleCannotInstallException($message);
+            throw new ModuleCannotInstallException((string) $message);
         }
         if (!$this->checkDependencies()) {
             $message = new Message(
                 $translator->translate('This module requires modules "%s".'), // @translate
                 implode('", "', $this->dependencies)
             );
-            throw new ModuleCannotInstallException($message);
+            throw new ModuleCannotInstallException((string) $message);
         }
         if (!$this->checkAllResourcesToInstall()) {
             $message = new Message(
                 $translator->translate('This module has resources that connot be installed.') // @translate
             );
-            throw new ModuleCannotInstallException($message);
+            throw new ModuleCannotInstallException((string) $message);
         }
         $this->execSqlFromFile($this->modulePath() . '/data/install/schema.sql');
         $this
@@ -184,6 +184,7 @@ abstract class AbstractModule extends \Omeka\Module\AbstractModule
         $form = $services->get('FormElementManager')->get($formClass);
         $form->init();
         $form->setData($data);
+        $form->prepare();
         return $renderer->formCollection($form);
     }
 
